cfd52d14a0d4fb4fafdb62a9fd32ed53dcff9499
leohchoi <choi.leo2002@gmail.com>
2023-08-22 09:13:45 -0400

    finished html part of createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FormField, Loader } from '../components';
+import Vite from '../assets/vite.svg'
 
 const CreatePost = () => {
   const navigate = useNavigate();
@@ -11,6 +12,10 @@ const CreatePost = () => {
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const generateImg = () => {
+
+  }
 
   const handleSubmit = () => {
     
@@ -47,6 +52,48 @@ const CreatePost = () => {
             value={form.prompt}
             handleChange={(e) => handleChange(e)}
           />
+          <div className='relative bg-gray-50 border border-rad-300 text-gray-900
+            text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 
+            h-64 flex justify-center items-center'>
+            {form.photo ? (
+              <img
+              src={form.photo}
+              alt={form.prompt}
+              className='w-full h-full object-contain'
+              />
+            ) : (
+              <img
+                src={Vite}
+                alt='Vite logo'
+                className='w-9/12 h-9/12 object-contain opacity-40'
+              />
+            )}
+            {generatingImg && (
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className='mt-5 flex gap-5'>
+          <button
+            type='button'
+            onClick={generateImg}
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[1rem]'>
+            Share Image w/community
+          </p>
+          <button
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {loading ? 'Sharing...' : 'Share w/ community'}
+          </button>
         </div>
       </form>
     </section>
